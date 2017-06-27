600308494c018a788f249309a79d0c86ccacf10b
Panagiotis Ktistakis <panktist@gmail.com>
2017-06-28 00:31:45 +0300

    Add tests for autotype

--- test_passata.py
+++ test_passata.py
@@ -590,3 +590,118 @@ def test_get_autotype(monkeypatch):
     entry = {'name': 'takis', 'pass': 'pass'}
     with pytest.raises(SystemExit):
         passata.get_autotype(entry)
+
+
+def test_autotype(monkeypatch, db):
+    monkeypatch.setattr(passata, 'active_window', lambda: (0, window_title))
+    monkeypatch.setattr(passata, 'call', lambda command: print(command))
+
+    window_title = 'GitHub: The world\'s leading blah blah blah'
+    result = run(['autotype'])
+    assert result.exception is None
+    assert result.output == (
+        "['xdotool', 'type', '--clearmodifiers', 'takis']\n"
+        "['xdotool', 'key', 'Tab']\n"
+        "['xdotool', 'type', '--clearmodifiers', 'gh']\n"
+        "['xdotool', 'key', 'Return']\n"
+    )
+
+
+def test_autotype_no_username(monkeypatch, db):
+    monkeypatch.setattr(passata, 'active_window', lambda: (0, window_title))
+    monkeypatch.setattr(passata, 'call', lambda command: print(command))
+
+    run(['insert', 'autotype/test1', '--password=pass1'])
+
+    window_title = 'test1'
+    result = run(['autotype'])
+    assert result.exception is None
+    assert result.output == (
+        "['xdotool', 'type', '--clearmodifiers', 'pass1']\n"
+        "['xdotool', 'key', 'Return']\n"
+    )
+
+
+def test_autotype_keywords_simple(monkeypatch, db):
+    monkeypatch.setattr(passata, 'active_window', lambda: (0, window_title))
+    monkeypatch.setattr(passata, 'call', lambda command: print(command))
+    monkeypatch.setattr(click, 'edit', lambda x, editor, extension: updated)
+
+    # Add a new entry with a single string in keywords
+    updated = (
+        'password: pass2\n'
+        'keywords: the keyword\n'
+    )
+    run(['edit', 'autotype/test2'])
+
+    window_title = 'A website with the keyword in its title'
+    result = run(['autotype'])
+    assert result.exception is None
+    assert result.output == (
+        "['xdotool', 'type', '--clearmodifiers', 'pass2']\n"
+        "['xdotool', 'key', 'Return']\n"
+    )
+
+
+def test_autotype_keywords_complex(monkeypatch, db):
+    monkeypatch.setattr(passata, 'active_window', lambda: (0, window_title))
+    monkeypatch.setattr(passata, 'call', lambda command: print(command))
+    monkeypatch.setattr(click, 'edit', lambda x, editor, extension: updated)
+
+    # Add a new entry with an autotype field and a list of
+    # strings in keywords that conflict with another entry.
+    updated = (
+        'username: user3\n'
+        'password: pass3\n'
+        'keywords:\n'
+        '- github\n'
+        '- yoyoyo\n'
+        'autotype: <username> Return !1.5 <password> Return\n'
+    )
+    run(['edit', 'autotype/test3'])
+
+    def fake_out(command, input):
+        print('Calling', command)
+        print('With input:')
+        print(input)
+        print()
+        return 'autotype/test3'
+
+    # Patch user interaction with dmenu to choose the new entry
+    # and sleeping to just print for how long it would sleep.
+    monkeypatch.setattr(passata, 'out', fake_out)
+    monkeypatch.setattr('time.sleep',
+                        lambda duration: print("Sleeping for", duration))
+
+    window_title = 'GitHub'
+    result = run(['autotype'])
+    assert result.exception is None
+    assert result.output == (
+        "Calling ['dmenu']\n"
+        "With input:\n"
+        "autotype/test3\n"
+        "internet/github\n"
+        "\n"
+        "['xdotool', 'type', '--clearmodifiers', 'user3']\n"
+        "['xdotool', 'key', 'Return']\n"
+        "Sleeping for 1.5\n"
+        "['xdotool', 'type', '--clearmodifiers', 'pass3']\n"
+        "['xdotool', 'key', 'Return']\n"
+    )
+
+
+def test_autotype_invalid(monkeypatch, db):
+    monkeypatch.setattr(passata, 'active_window', lambda: (0, window_title))
+    monkeypatch.setattr(click, 'edit', lambda x, editor, extension: updated)
+    # Suppress notification
+    monkeypatch.setattr(passata, 'call', lambda command: None)
+
+    updated = (
+        'password: pass4\n'
+        'autotype: <username> Tab <password> Return\n'
+    )
+    run(['edit', 'autotype/test4'])
+
+    window_title = 'test4'
+    result = run(['autotype'])
+    assert repr(result.exception) == 'SystemExit(1,)'
