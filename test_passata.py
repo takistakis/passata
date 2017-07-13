1afe9796c57f45d781aebb2165916b0b114c0652
Panagiotis Ktistakis <panktist@gmail.com>
2017-07-13 23:50:27 +0300

    Make list dashes red when showing with color
    Also add a test for showing with and without color

--- test_passata.py
+++ test_passata.py
@@ -17,6 +17,7 @@
 
 """Tests for passata."""
 
+import functools
 import os
 import time
 
@@ -196,6 +197,51 @@ def test_show(db):
 
     # Show group with trailing slash
     result = run(['show', 'internet/'])
+    assert result.output == expected
+
+
+def test_show_color(monkeypatch, db):
+    # Do not strip ANSI codes automatically
+    color_echo = functools.partial(click.echo, color=True)
+    monkeypatch.setattr(click, 'echo', color_echo)
+
+    # Insert a new entry with a list
+    monkeypatch.setattr(click, 'edit', lambda x, editor, extension: updated)
+    updated = (
+        'username: user\n'
+        'password: pass\n'
+        'autotype: <username> Return !1.5 <password> Return\n'
+        'keywords:\n'
+        '- youtube\n'
+        '- gmail\n'
+    )
+    run(['edit', 'group/google'])
+
+    # Test show without color
+    expected = (
+        'google:\n'
+        '  username: user\n'
+        '  password: pass\n'
+        '  autotype: <username> Return !1.5 <password> Return\n'
+        '  keywords:\n'
+        '  - youtube\n'
+        '  - gmail\n'
+    )
+    result = run(['show', 'group', '--no-color'])
+    assert result.output == expected
+
+    # Test show with color
+    expected = (
+        '\033[38;5;12mgoogle\033[38;5;11m:\033[0m\n'
+        '\033[38;5;12m  username\033[38;5;11m:\033[0m user\n'
+        '\033[38;5;12m  password\033[38;5;11m:\033[0m pass\n'
+        '\033[38;5;12m  autotype\033[38;5;11m:\033[0m '
+        '<username> Return !1.5 <password> Return\n'
+        '\033[38;5;12m  keywords\033[38;5;11m:\033[0m\n'
+        '\033[38;5;9m  - \033[0myoutube\n'
+        '\033[38;5;9m  - \033[0mgmail\n'
+    )
+    result = run(['show', 'group', '--color'])
     assert result.output == expected
 
 
